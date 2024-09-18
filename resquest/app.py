4d58e6d8fd10349f4a8e50d6f418d3fe6ebfc58b
Marquin <122325944+Estranho2023@users.noreply.github.com>
2024-09-18 09:00:32 -0300

    Update app.py

--- resquest/app.py
+++ resquest/app.py
@@ -13,18 +13,20 @@ def cadastro():
 
 @app.route('/dados', methods = ['GET', 'POST']) #Digo pra rota que ela vai ter 2 methodos (GET e POST)
 def dados():
-  # Se o methodo for GET, a requisição foi feita pela url 
-  # Logo o sistema não vai permitir isso e vai retorna o usuário novamente pra pagina de cadastro.
+    # Se o methodo for GET, a requisição foi feita pela url 
+    # Logo o sistema não vai permitir isso e vai retorna o usuário novamente pra pagina de cadastro.
     if request.method == 'GET':
-      # Retorna o usuário para cadastro
+        # Retorna o usuário para cadastro
         return redirect(url_for('cadastro'))
 
   # Se a requisição for POST o sistema continua com seu fluxo
     else:
-    # Pegando email e senha do formulário
+        # Pegando o valor do email do form, usando o nome que foi dado no input ('f_email")
         email = request.form['f_email']
+        # Pega o valor da senha do form, usando o nome que foi dado no input ('f_senha')
         senha = request.form['f_senha']
-    # Aqui deu pau
+        
+        # Aqui deu pau
         dados = {}
         dados['email'] = email
         dados['senha'] = senha
