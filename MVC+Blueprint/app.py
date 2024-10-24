89b9078f79b64742a410ff75e567427d01477eb8
Marquin <122325944+Estranho2023@users.noreply.github.com>
2024-10-24 14:16:10 -0300

    Update app.py

--- MVC+Blueprint/app.py
+++ MVC+Blueprint/app.py
@@ -1,6 +1,15 @@
 from flask import Flask
+# Importando da pasta controllers (Biblioteca) os arquivo users e books (Funções)
 from controllers import users, books
 
+"""Ok, oque é o MVC?
+MVC é a sigla pra Model Viwer Control, isso não é código e sim a forma de organização do código. 
+"""
+
+# Instância do Flask
 app = Flask(__name__)
+
+# Ainda não sei
+# EXPICAR!!!
 app.register_blueprint(users.bp)
 app.register_blueprint(books.bp)
