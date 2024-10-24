025d3f20745d98b7e9dbb1125b0f777206c30f05
Marquin <122325944+Estranho2023@users.noreply.github.com>
2024-10-24 14:27:21 -0300

    Update __init__.py

--- MVC+Blueprint/controllers/__init__.py
+++ MVC+Blueprint/controllers/__init__.py
@@ -1,4 +1,6 @@
-
+""" O arquivo __init__.py serve como o arquivo "Contrutor" igual em POO, tendo dentro dele a variável 
+"all", que serve para definir quai modulos, classes, funções ou objetos podem ser importados quanto utilizado
+o comando import"""
 all = [
     'books',
     'users'
