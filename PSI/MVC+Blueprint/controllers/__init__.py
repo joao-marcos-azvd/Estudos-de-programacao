9db24ef53a89d641f78c58ab2d2fe4f733fa6291
Marquin <122325944+Estranho2023@users.noreply.github.com>
2024-10-26 07:27:39 -0300

    Add files via upload

---
+++ PSI/MVC+Blueprint/controllers/__init__.py
@@ -0,0 +1,7 @@
+""" O arquivo __init__.py serve como o arquivo "Contrutor" igual em POO, tendo dentro dele a variável 
+"all", que serve para definir quai modulos, classes, funções ou objetos podem ser importados quanto utilizado
+o comando import"""
+all = [
+    'books',
+    'users'
+]
