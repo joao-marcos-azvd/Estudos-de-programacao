7b0fbd2af91470946a32f415f53b6e9306680239
Marquin <122325944+joao-marcos-azvd@users.noreply.github.com>
2024-11-28 08:05:55 -0300

    Update minha_parte.sql

--- Banco_Dados/feito_em_sala/Ajeitar/minha_parte.sql
+++ Banco_Dados/feito_em_sala/Ajeitar/minha_parte.sql
@@ -6,15 +6,53 @@ select * from tb_notas;
 select * from tb_alunos;
 select * from tb_matriculas;
 
+-- Crie uma função que receba o ‘id do curso’ e retorne um texto com as seguintes informações do curso: 
+
+-- Percentual de alunos aprovados;
+create function 
+fn_mpond (n1 float, n2 float, n3 float, n4 float)
+returns varchar(10)
+return round((n1 * 2 + n2 * 3 + n3 * 4 + n4 * 5)/14, 2);
+
+
+delimiter //
+create function fn_situacao(mediaPonderada float) 
+returns varchar(45)
+begin
+    if mediaPonderada>=6 then return 'Aprovado';
+    elseif mediaPonderada <=3.5 THEN return 'Reprovado';
+    else return 'Prova Final';
+    end if;
+end //
+delimiter ;
+
+set @qntalunos = 
+(select count(mat_id) from tb_matriculas 
+where mat_curso_id = 2);
+
+select alu_id from tb_alunos join tb_matriculas
+on mat_alu_id = alu_id
+where mat_curso_id = 2;
+
+-- Percentual de alunos reprovados;
+-- Quantidade de disciplinas;
+-- Média geral do curso;
+-- Quantidade de alunos acima da média do curso.
+
 -- Fazer 
-select count(mat_id) from tb_matriculas join tb_cursos
-on 2 = mat_curso_id;
+(select count(mat_id) from tb_matriculas 
+where mat_curso_id = id);
 
 delimiter !
 create function fn_info_cursos (id int)
 returns varchar (1000)
 begin
-set @num_matriculas = (select count(mat_id) from tb_matriculas);
+set @qntalunos = 
+(select count(mat_id) from tb_matriculas 
+where mat_curso_id = id);
+
+set @per_aprovados = 
+"fazendo";
 
 end!
 delimiter ;
