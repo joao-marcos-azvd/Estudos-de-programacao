bd21ce6a172ae53b37bb54686860433a0548a73f
Marquin <122325944+joao-marcos-azvd@users.noreply.github.com>
2024-12-15 18:08:22 +0000

    Projeto de redes com flask

--- Redes/projeto_flask/app.py
+++ Redes/projeto_flask/app.py
@@ -1,3 +1,10 @@
-from flask import Flask
+# Importando coisas do Flask
+from flask import Flask, render_template
+# Importando sqlite3
+import sqlite3
 
 app = Flask(__name__)
+
+@app.route('/')
+def index():
+    return render_template('index.html')
